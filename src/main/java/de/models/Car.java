8143236fe3ddab7bb481a7cff3439884735c07a6
AIRAT1 <ayrat1@mail.ru>
2020-03-30 23:14:01 +0300

    1.32.32

--- src/main/java/de/models/Car.java
+++ src/main/java/de/models/Car.java
@@ -2,13 +2,22 @@ package de.models;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @ToString(exclude = "owner")
+@Entity
+@Table(name = "fix_car")
 public class Car {
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String model;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private User owner;
 }
