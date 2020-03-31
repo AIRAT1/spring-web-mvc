7f86208501b0d5cf6c091e0fe283e84968cdafbc
AIRAT1 <ayrat1@mail.ru>
2020-03-31 22:38:00 +0300

    1.49.00

---
+++ src/main/java/de/controllers/CarsController.java
@@ -0,0 +1,26 @@
+package de.controllers;
+
+import de.models.Car;
+import de.repositories.CarsRepository;
+import de.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class CarsController {
+    @Autowired
+    private CarsRepository carsRepository;
+
+    @RequestMapping(path = "/cars", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCarsByOwnerFirstName(@RequestParam(name = "first_name") String firstName){
+        List<Car> cars = carsRepository.findAllByOwner_FirstName(firstName);
+        return cars.toString();
+    }
+}
